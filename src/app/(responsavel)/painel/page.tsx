c5ce9f437a15c75c75141eb4a22f7e8f969e28c8
maurocastrout-crypto <maurocastrout@gmail.com>
2026-04-09 10:08:36 -0300

    Update page.tsx

--- src/app/(responsavel)/painel/page.tsx
+++ src/app/(responsavel)/painel/page.tsx
@@ -1,4 +1,4 @@
-// src/app/(responsavel)/painel/page.tsx
+(responsavel)/responsavel/painel/page.tsx
 import { createClient } from '@/lib/supabase/server'
 import { AREAS_ENEM, type AreaENEM } from '@/types'
 import { format } from 'date-fns'
